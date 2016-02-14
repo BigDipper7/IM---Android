dd46895466cf1a6982c974e0144241feab24e17e
Minghao Xu <violiniselegant@gmail.com>
2016-02-14 11:32:57 +0800

    just change all must change key and make it can be used, good , and exist some bug, needed change and fix

--- server-java/src/org/pushtalk/server/Config.java
+++ server-java/src/org/pushtalk/server/Config.java
@@ -5,8 +5,8 @@ public class Config {
     // Should be assign value when db init
     public static String SERVER_ID;
     
-    public static final String JPUSH_MASTER_SECRET = "5e987ac6d2e04d95a9d8f0d1";
-    public static final String JPUSH_APPKEY = "7d431e42dfa6a6d693ac2d04";
+    public static final String JPUSH_MASTER_SECRET = "824949afc63285a1fa48e184";
+    public static final String JPUSH_APPKEY = "43fde850329a226aa5536c6f";
     
     public static final String VERSION = "0.2.3";
     
